c7c6613ec51dc679e6a9f966b690acde66856b34
Dmitry <dc914337@gmail.com>
2016-02-15 13:32:50 +0300

    initial

---
+++ PsdBasesSetter/PsdBasesSetter/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PsdBasesSetter.Repositories
+{
+    public interface IRepository
+    {
+        bool WriteChanges();
+        
+
+        DateTime LastUpdated { get; }
+    }
+}
